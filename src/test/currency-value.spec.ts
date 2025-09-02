1ea595822c244d72c2a3f7e1adc8741823af4e75
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-02 15:03:32 +0000

    test: update.

--- src/test/currency-value.spec.ts
+++ src/test/currency-value.spec.ts
@@ -5,12 +5,19 @@ import {
   Exchange,
 } from "../lib";
 
-// const v = new CurrencyValue(42345255.356, 'USD');
+console.group(`CurrencyValue`);
 
-// console.log(`v.valueOf(): `, v);
-// console.log(`v.valueOf(): `, v.valueOf());
-// console.log(`v.currency: `, v.currency);
-// console.log(`v.value: `, v.of);
-// console.log(`v.getValueOfCurrency('USD'):`, v.getValueAsCurrency(''));
+const value = new CurrencyValue(42345255.356, 'USD');
 
-// console.log('');
+console.log(`value: `, value); // 
+console.log(`value.valueOf(): `, value.valueOf()); // 42345255.356
+console.log(`value.currency: `, value.currency); // USD
+console.log(`value.currencySymbol: `, value.currencySymbol); // $
+console.log(`value.formatted: `, value.formatted); // 42,345,255.36
+
+console.log(`value.of: `, value.of); // 42345255.356
+console.log(`value.getValueOfCurrency('USD'):`, value.getValueAsCurrency('USD')); // $42,345,255.36
+console.log('value.isBetweenEvery([43345255, 52345255.356]).', value.isBetweenEvery([43345255, 52345255.356])); // false
+console.log(`value.lessThan(52345255.356):`, value.lessThan(52345255.356)); // true
+
+console.groupEnd();
