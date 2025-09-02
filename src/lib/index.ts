59ba68742abb3e7d976ed9a19041eaf8651d94d5
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-02 10:37:34 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
-export { Conversion } from './conversion.abstract';
+
+export { Conversion } from './conversion.class';
+export { ConversionBase } from './conversion-base.abstract';
 export { Currencies } from './currencies.class';
 export { CurrencyValue } from './currency-value.class';
 export { Exchange } from './exchange.class';
-
