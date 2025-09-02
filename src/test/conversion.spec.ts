aeed57598842f177e74b167ec781e8b876ed4dae
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-09-02 14:35:43 +0000

    test: update.

--- src/test/conversion.spec.ts
+++ src/test/conversion.spec.ts
@@ -1,5 +1,35 @@
-import { Conversion } from "../lib";
+import { Conversion, Exchange } from "../lib";
 import { CONVERSION_RATES } from "./conversion.rates";
+
+console.group(`Conversion`);
+
+const conversion = new Conversion(15 as number, 'PLN', { 'USD': 0.275, 'EUR': 0.2347 });
+
+console.debug(`conversion.amount`, conversion.amount); // 15
+console.debug(`conversion.currency`, conversion.currency); // PLN
+console.debug(`conversion.currencies`, conversion.currencies); // ['USD', 'EUR']
+
+console.debug(`conversion.getConversionRates()`, conversion.getConversionRates()); // { 'USD': 0.275, 'EUR': 0.2347 }
+console.debug(`conversion.conversionRates`, conversion.conversionRates); // Map([["USD", 0.275], ["EUR", 0.2347 ]]);
+
+console.debug(`conversion.to('USD')`, conversion.to('USD')); // 4.125
+console.debug(`conversion.to('EUR', 15)`, conversion.to('EUR', 15)); // 3.5204999999999997
+
+console.debug(`conversion.toMany(['USD', 'EUR'])`, conversion.toMany(['USD', 'EUR'])); // { USD: 4.125, EUR: 3.5204999999999997 }
+console.debug(`conversion.toMany(['USD', 'EUR'], 10)`, conversion.toMany(['USD', 'EUR'], 10)); // { USD: 2.75, EUR: 2.347 }
+console.debug(`conversion.sumTo(['USD', 'EUR'], 10)`, conversion.sumTo(['USD', 'EUR'], 10)); // 5.0969999999999995
+
+console.debug(`conversion.from('USD')`, conversion.from('USD')); // 54.54545454545454
+console.debug(`conversion.from('EUR', 15)`, conversion.from('EUR', 15)); // 63.911376224968045
+console.debug(`conversion.sumFrom(['USD', 'EUR'], 10)`, conversion.sumFrom(['USD', 'EUR'], 10)); // 78.97122051361507
+
+console.debug(`conversion.fromMany(['USD', 'EUR'])`, conversion.fromMany(['USD', 'EUR'])); // { USD: 54.54545454545454, EUR: 63.911376224968045 }
+console.debug(`conversion.fromMany(['USD', 'EUR'], 10)`, conversion.fromMany(['USD', 'EUR'], 10)); // { "USD": 36.36363636363636, "EUR": 42.6075841499787 }
+
+console.debug(`conversion.setAmount(5)`, conversion.setAmount(5)); // 
+console.debug(`conversion.to('EUR')`, conversion.to('EUR')); // 1.1735
+
+console.groupEnd();
 
 describe(`Conversion`, () => {
   let conversion = new Conversion(15 as number, 'PLN');
--- src/test/exchange.spec.ts
+++ src/test/exchange.spec.ts
@@ -1,9 +1,27 @@
 import { Exchange } from "../lib";
 import { CONVERSION_RATES } from "./conversion.rates";
 
+console.group(`Exchange`);
+
+const exchange = new Exchange(15 as number, 'PLN', ['USD', 'EUR'], { 'USD': 0.275, 'EUR': 0.2347 });
+
+console.debug(`exchange.amount`, exchange.amount); // 15
+console.debug(`exchange.currency`, exchange.currency); // PLN
+console.debug(`exchange.currencies`, exchange.currencies); // ['USD', 'EUR']
+
+console.debug(`exchange.getConversionRates()`, exchange.exchangeRates); // { 'USD': 0.275, 'EUR': 0.2347 }
+
+exchange.to('USD').then(result => console.debug(`exchange.to('USD')`, result)); // 4.125
+exchange.to('EUR', 15).then(result => console.debug(`exchange.to('EUR', 15)`, result)); // 3.5204999999999997
+
+exchange.from('USD').then(result => console.debug(`exchange.from('USD')`, result)); // 54.54545454545454
+exchange.from('EUR', 15).then(result => console.debug(`exchange.from('EUR', 15)`, result)); // 63.911376224968045
+
+console.groupEnd();
+  
 describe(`Exchange`, () => {
-  let exchange = new Exchange(15 as number, 'PLN');
-  beforeEach(() => exchange = new Exchange(15 as number, 'PLN', CONVERSION_RATES))
+  let exchange = new Exchange(15 as number, 'PLN', ['USD', 'EUR']);
+  beforeEach(() => exchange = new Exchange(15 as number, 'PLN', ['USD', 'EUR'], CONVERSION_RATES))
 
   it(`should properly calculate in to method`, async () => {
     await expectAsync(exchange.to('USD')).toBeResolvedTo(4.125);
